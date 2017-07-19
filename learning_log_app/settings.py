062a7e0d166e230b6e524f874e437c96948abea4
akash <agarwal.akash1997@gmail.com>
2017-07-19 12:23:24 +0530

    Ready for deployment to heroku.

--- learning_log_app/settings.py
+++ learning_log_app/settings.py
@@ -146,7 +146,7 @@ if os.getcwd() == '/app':
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR,'static'),
+        os.path.join(BASE_DIR,'/learning_log_app/static'),
     )
