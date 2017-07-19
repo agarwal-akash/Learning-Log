b423adcf04b3d0f5b0d5b32799b9c98f2eb9ecfd
akash <agarwal.akash1997@gmail.com>
2017-07-19 11:56:12 +0530

    Ready for deployment to heroku.

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+""" Defines URL patterns for users"""
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    # login page
+    url(r'^login/$',login,{'template_name':'users/login.html'},
+        name='login'),
+    # logout page
+    url(r'^logout/$',views.logout_view,name='logout'),
+    # Registration page
+    url(r'^register/$',views.register,name='register')
+]
